5893299121cd6b3ad7daf0795af6e5271bba2294
muxazzXJohny <maracoco334@gmail.com>
2024-12-03 15:34:05 +0200

    task-2

---
+++ ConsoleApp11-2-classwork/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11_2_classwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String st1 = "1";
+            String st2 = "2";
+            String st3 = "3";
+            String st4 = "4";
+            Console.WriteLine($"Вывод перед обменом:{st1} {st2} {st3} {st4}");
+            Swap(ref st1, ref st2);
+            Console.WriteLine($"Вывод после обмена:{st1}+{st2}");
+            Console.ReadKey();
+        }
+        static void Swap(ref string x, ref string y)
+        {
+            String p = x;
+            x = y;
+            y = p;
+        }
+
+
+
+    }
+}
+
