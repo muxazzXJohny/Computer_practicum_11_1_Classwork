d8e0b91fd46b452d113e797140c1ce5c7579b122
muxazzXJohny <maracoco334@gmail.com>
2024-12-03 09:12:08 +0200

    task-1

---
+++ ConsoleApp03.12.24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp03._12._24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String st1 = "Это первая строка";
+            String st2 = "Это вторая строка";
+            Console.WriteLine($"Вывод перед обменом:{st1} + {st2}");
+            Swap(ref st1, ref st2);
+            Console.WriteLine($"Вывод после обмена:{st1}+{st2}");
+            Console.ReadKey();
+        }
+        static void Swap(ref string x, ref string y)
+        {
+            String p = x;
+            x = y;
+            y = p;
+        }
+
+    }
+}
